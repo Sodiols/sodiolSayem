a09c1705b833c23932333dbeae144034884fd9ce
Sodiol Sayem <134397635+Sodiols@users.noreply.github.com>
2024-06-13 20:53:47 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import About from "./Components/About/About"
 import Hero from "./Components/Hero/Hero"
 import LeftNavbar from "./Components/LeftNavbar/LeftNavbar"
-import Services from "./Components/Services/Services"
 import Skills from "./Components/Skills/Skills"
+import Services from './Components/Services/Services';
 
 
 function App() {
